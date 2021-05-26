3fcd8f56642bc3108c0d4cf7f18f6991aaac5585
RoarshacH <smv.dilshan@gmail.com>
2021-05-26 18:27:42 +0530

    Solution Setup

---
+++ MyShop/MyShop.WEBUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.WEBUI.Startup))]
+namespace MyShop.WEBUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
