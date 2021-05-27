fd09aadd3bb24e3ef9a0bb98cf380593f8b0e411
RoarshacH <smv.dilshan@gmail.com>
2021-05-27 14:03:17 +0530

    Product Categories Add and View Model

--- MyShop/MyShop.WEBUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WEBUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WEBUI.Controllers
@@ -11,9 +12,11 @@ namespace MyShop.WEBUI.Controllers
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController() {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
 
         // GET: ProductManager
@@ -25,8 +28,10 @@ namespace MyShop.WEBUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();            
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -52,7 +57,11 @@ namespace MyShop.WEBUI.Controllers
                 return HttpNotFound();
             }
             else {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                
+                return View(viewModel);
             }
             
         }
